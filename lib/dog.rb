a6dd284a6538325db9a4944c79421f72be4a17be
Andrew Zajac <ajzajac@icloud.com>
2019-08-01 19:05:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,16 @@ class Dog
   def initialize(name)
     @name = name 
   end
+  
   def name=(dogname)
-    
+    @name = dogname
+  end
+  
+  def breed(breed)
+    @breed = breed
+  end
+  
+  def breed=(breedname)
+    @breed = breedname
+  end
 end
